8d133583050fe93dcbf542e3c0e7f7d790f21f02
Andrei Zanouski <andreizanik@gmail.com>
2019-06-22 13:44:44 +0300

    getCookies, setCookies, removeCookies

---
+++ index.js
@@ -0,0 +1,96 @@
+import cookie from 'cookie';
+
+const isClientSide = () => {
+	return typeof window !== 'undefined';
+};
+
+const processValue = (value) => {
+	if (value === 'true') return true;
+	if (value === 'false') return false;
+	if (value === 'undefined') return undefined;
+	if (value === 'null') return null;
+
+	return value;
+};
+
+const stringify = (value = '') => {
+	try {
+		const result = JSON.stringify(value);
+		return (/^[\{\[]/.test(result)) ? result : value;
+	} catch {
+		return value;
+	}
+};
+
+const decode = (str) => {
+	if (!str) return str;
+
+	return str.replace(/(%[0-9A-Z]{2})+/g, decodeURIComponent);
+};
+
+export const getCookies = (ctx = null, key) => {
+	if (!isClientSide()) {
+		// if cookie-parser is used in project get cookies from ctx.req.cookies
+		// if cookie-parser isn't used in project get cookies from ctx.req.headers.cookie
+		if (ctx && ctx.req && ctx.req.cookies) {
+			return key ? processValue(ctx.req.cookies[key]) : ctx.req.cookies;
+		}
+
+		if (ctx && ctx.req && ctx.req.headers && ctx.req.headers.cookie) {
+			const _cookies = cookie.parse(ctx.req.headers.cookie);
+			return key ? processValue(_cookies[key]) : _cookies;
+		}
+
+		return undefined;
+	}
+
+	const _cookies = {};
+	const documentCookies = document.cookie ? document.cookie.split('; ') : [];
+
+	for (let i = 0; i < documentCookies.length; i++) {
+		const cookieParts = documentCookies[i].split('=');
+
+		const _cookie = cookieParts.slice(1).join('=');
+		const name = cookieParts[0];
+
+		_cookies[name] = _cookie;
+	}
+
+	return key ? processValue(decode(_cookies[key])) : _cookies;
+};
+
+export const setCookies = (ctx = null, key, data, options = {}) => {
+	if (typeof options.expires === 'number') {
+		options.expires = new Date(new Date() * 1 + options.expires * 864e+5);
+	}
+
+	const cookieStr = cookie.serialize(key, stringify(data), { path: '/', ...options });
+
+	if (!isClientSide()) {
+		if (ctx && ctx.res) {
+			const currentCookies = ctx.res.getHeader('Set-Cookie');
+
+			if (!currentCookies) {
+				return ctx.res.setHeader(
+					'Set-Cookie',
+					[cookieStr]
+				);
+			}
+
+			return ctx.res.setHeader(
+				'Set-Cookie',
+				currentCookies.concat(cookieStr),
+			);
+		}
+		return undefined;
+	}
+
+	document.cookie = cookieStr;
+};
+
+// if cookie set with path then need remove cookie with this path
+export const removeCookies = (ctx = null, key, options = {}) => {
+	return setCookies(ctx, key, '', { ...options, expires: -1 });
+};
+
+export default { getCookies, setCookies, removeCookies}
