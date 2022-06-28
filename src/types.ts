3a75e8cf032c87cd8a6a61bc7fb6ca7224542eb4
mxgnus <84542021+mxgnus-de@users.noreply.github.com>
2022-06-28 22:25:11 +0200

    fix "OptionsType" typeerror

--- src/types.ts
+++ src/types.ts
@@ -4,9 +4,9 @@ import { IncomingMessage, ServerResponse } from "http";
 export interface OptionsType extends CookieSerializeOptions {
     res?: ServerResponse;
     req?: IncomingMessage & {
-        cookies?:{ [key: string]: string; }
+        cookies?:{ [key: string]: string; } | Partial<{ [key: string]: string}>
     }
 }
 
-export type TmpCookiesObj = { [key: string]: string };
+export type TmpCookiesObj = { [key: string]: string } | Partial<{ [key: string]: string}>;
 export type CookieValueTypes = string | boolean | undefined | null;
