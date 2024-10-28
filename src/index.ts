6c37bc35e6c2f0513502739ef64915ec53383e98
Grzegorz Dubiel <greg2012201@gmail.com>
2024-10-28 22:15:43 +0100

    refactor: Rebuild smart export to serve functions correctly.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,41 @@
 import * as clientCookies from './client';
 import * as serverCookies from './server';
 export * from './common/types';
+import type { OptionsType } from './common/types';
+import { isClientSide } from './common/utils';
 
-// Re-export individual functions for backwards compatibility
-export const { getCookie, getCookies, setCookie, deleteCookie, hasCookie } =
-  typeof window === 'undefined' ? serverCookies : clientCookies;
+const getRenderPhase = () => (typeof window === 'undefined' ? 'server' : 'client');
+
+const getFn = (fnName: keyof typeof clientCookies, options?: OptionsType) => {
+  if (isClientSide(options)) {
+    if (getRenderPhase() === 'server') {
+      // to prevent crash caused by missing window.document during server rendering phase
+      return;
+    }
+    return clientCookies[fnName];
+  }
+
+  return serverCookies[fnName];
+};
+export const getCookies = (options?: OptionsType) => {
+  const fn = getFn('getCookies', options) as (
+    options?: OptionsType,
+  ) => Promise<serverCookies.TmpCookiesObj> | clientCookies.TmpCookiesObj;
+  if (!fn) {
+    return;
+  }
+
+  return fn(options);
+};
+export const getCookie = (key: string, options?: OptionsType) => {
+  return getFn('getCookie', options)?.(key, options);
+};
+export const setCookie = (key: string, data: any, options?: OptionsType) => {
+  return getFn('setCookie', options)?.(key, data, options);
+};
+export const deleteCookie = (key: string, options?: OptionsType) => {
+  return getFn('setCookie', options)?.(key, options);
+};
+export const hasCookie = (key: string, options?: OptionsType) => {
+  return getFn('hasCookie', options)?.(key, options);
+};
