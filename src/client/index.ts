b860cfde94810137e9b5b0fa7dc226890068848c
Grzegorz Dubiel <greg2012201@gmail.com>
2024-10-31 21:45:08 +0100

    fix:Put the server phase guard in the right places.

--- src/client/index.ts
+++ src/client/index.ts
@@ -4,18 +4,17 @@ import { stringify, decode, isClientSide, getRenderPhase } from '../common/utils
 
 const ensureClientSide = (options?: OptionsType) => {
   if (!isClientSide(options)) {
-    if (getRenderPhase() === 'server') {
-      // to prevent crash caused by missing window.document during server rendering phase
-      return;
-    }
     throw new Error(
       'You are trying to access cookies on the server side. Please, use the server-side import with `cookies-next/server` instead.',
     );
   }
 };
 
-const getCookies = (_options?: OptionsType): TmpCookiesObj => {
+const getCookies = (_options?: OptionsType): TmpCookiesObj | undefined => {
   ensureClientSide(_options);
+  if (getRenderPhase() === 'server') {
+    return;
+  }
   const cookies: TmpCookiesObj = {};
   const documentCookies = document.cookie ? document.cookie.split('; ') : [];
 
@@ -31,15 +30,17 @@ const getCookies = (_options?: OptionsType): TmpCookiesObj => {
 
 const getCookie = (key: string, options?: OptionsType): CookieValueTypes => {
   ensureClientSide(options);
-
   const _cookies = getCookies(options);
-  const value = _cookies[key];
+  const value = _cookies?.[key];
   if (value === undefined) return undefined;
   return decode(value);
 };
 
 const setCookie = (key: string, data: any, options?: OptionsType): void => {
   ensureClientSide(options);
+  if (getRenderPhase() === 'server') {
+    return;
+  }
   const _cookieOptions = options || {};
   const cookieStr = serialize(key, stringify(data), { path: '/', ..._cookieOptions });
   document.cookie = cookieStr;
@@ -54,6 +55,9 @@ const hasCookie = (key: string, options?: OptionsType): boolean => {
   ensureClientSide(options);
   if (!key) return false;
   const cookies = getCookies(options);
+  if (!cookies) {
+    return false;
+  }
   return Object.prototype.hasOwnProperty.call(cookies, key);
 };
 
