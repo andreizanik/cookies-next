131b33dc2fbcaa5ca8389a3218114f52290badb1
Grzegorz Dubiel <greg2012201@gmail.com>
2025-05-31 21:52:08 +0200

    test: Add one more test for cookies state revalidation.

--- src/client/reactive-hooks.test.tsx
+++ src/client/reactive-hooks.test.tsx
@@ -273,4 +273,25 @@ describe('Reactive hooks operations.', () => {
     });
     expect(cookies).toEqual({ testKey1: 'updatedValue1', testKey2: 'testValue2' });
   });
+  test('should reflect cookie deletion', () => {
+    document.cookie = 'testKey1=testValue1';
+    document.cookie = 'testKey2=testValue2';
+    const { result } = renderHook(() => useReactiveCookiesNext(), { wrapper: createWrapper() });
+
+    let cookies = null;
+    act(() => {
+      cookies = result.current.getCookies();
+    });
+    expect(cookies).toEqual({ testKey1: 'testValue1', testKey2: 'testValue2' });
+
+    document.cookie = `testKey1=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/`;
+    act(() => {
+      result.current.revalidateCookiesState();
+    });
+
+    act(() => {
+      cookies = result.current.getCookies();
+    });
+    expect(cookies).toEqual({ testKey2: 'testValue2' });
+  });
 });
